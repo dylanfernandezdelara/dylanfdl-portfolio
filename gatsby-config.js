f27349ed1b71f2eb155265fd76c530db5d118810
dylanfernandezdelara <dylanfernandezdelara@yale.edu>
2021-01-22 11:14:24 -0600

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-145039616-1",
+        trackingId: "",
         head: true,
       },
     },
